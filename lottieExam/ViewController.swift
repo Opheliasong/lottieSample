23e39ba514faa7d932c0ab86558c45b896e4fa51
SungMin Park <nervous@bloomsburypartners.com>
2021-03-11 23:47:02 +0900

    Initial Commit

---
+++ lottieExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lottieExam
+//
+//  Created by Sung Min Park on 2021/03/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
